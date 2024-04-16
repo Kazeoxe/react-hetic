3c0944723b9c8e395de45bcc25c66f529274a838
Samuel Lunion <lunion.sam@gmail.com>
2024-04-16 09:49:13 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({  base: "/hetic_react/",  plugins: [react()],})
+export default defineConfig({  base: "/react-hetic/",  plugins: [react()],})
